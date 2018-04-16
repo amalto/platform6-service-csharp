056ce4cf0b34c8107576dca4f3c1c66c19873d47
Roxane Macé <roxane.mace@isep.fr>
2018-04-16 11:28:26 +0200

    Add `Platform6.Client` package

--- Platform6/Program.cs
+++ Platform6/Program.cs
@@ -1,5 +1,34 @@
-﻿namespace Service {
+﻿using System;
+using System.Collections.Generic;
+using Library.Models;
+
+namespace Service {
 	internal class Program {
-		public static void Main(string[] args) { }
+		private const string MyServiceId = "demo.csharp";
+
+		public static void Main() {
+			var parameters = new DeployParameters {
+				Id = MyServiceId,
+				Username = "admin@amalto.com",
+				Path = "/" + MyServiceId + "/api/",
+				BasePath = "http://docker.for.mac.localhost:8000",
+				Versions = new Versions {
+					Client = "0.0.0",
+					Server = "0.0.1"
+				},
+				Ui = new UserInterfaceProperties {
+					visible = true,
+					iconName = "fas fa-hashtag",
+					weight = 30,
+					label = new Dictionary<string, string> {
+						{"en-US", "C Sharp"},
+						{"fr-FR", "C Sharp"}
+					}
+				}
+			};
+
+			var service = new Library.Service(parameters);
+			service.Deployed.ContinueWith(Console.WriteLine);
+		}
 	}
 }
